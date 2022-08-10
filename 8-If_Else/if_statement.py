73587ddabe6aa1c612e278b8144c286487e14450
Amit <Amitshr6779@gmail.com>
2022-08-10 16:54:37 +0530

    Added ques in if_statement.py file

--- 8-If_Else/if_statement.py
+++ 8-If_Else/if_statement.py
@@ -43,3 +43,30 @@ def max_value(num1, num2, num3):
         return num3
 
 print(max_value(2, 8, 19))
+
+'''
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+'''
+n = int(input("Enter number between 1 to 100").strip())
+if n in range(2,5) and n % 2 == 0:
+    print("Not weired")
+elif n in range(6,20) and n % 2 ==0:
+    print("weird")
+elif n > 20 and n % 2 ==0:
+    print("Not weird")
+else:
+    print("Invalid number weird")   
+
+
+#Alternative of above questions:
+if __name__ == '__main__':
+    n = int(input("Enter number"))
+    if n % 2 != 0 or n > 5 and n < 21 :
+        print("Weird")
+    else: 
+        print("Not Weird")
